c6e6df9be160b997ea5e6ac7463d18aa8df69148
1기_1회차_주세환 <j4615545@gmail.com>
2022-09-23 16:41:47 +0900

    [Silver IV] Title: 좋은 단어, Time: 148 ms, Memory: 115444 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+cnt = 0
+
+for _ in range(N):
+    good = input()
+    stack = []
+    for i in good:
+        if len(stack) == 0 :
+            stack.append(i)
+        else:
+            if i == "A":
+                if stack[-1] == "B":
+                    stack.append(i)
+                elif stack[-1] =="A":
+                    stack.pop()
+            elif i == "B":
+                if stack[-1] == "A":
+                    stack.append(i)
+                elif stack[-1] == "B":
+                    stack.pop()
+    if len(stack) == 0:
+        cnt += 1
+
+
+print(cnt)            
