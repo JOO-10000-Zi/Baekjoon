6ed0d3b5719044154aee68949006e988e5b3e1e0
1기_1회차_주세환 <j4615545@gmail.com>
2022-10-03 15:55:32 +0900

    [D2] Title: 초심자의 회문 검사, Time: 197 ms, Memory: 58,312 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -0,0 +1,12 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    # ///////////////////////////////////////////////////////////////////////////////////
+    name = input()
+    
+    r = reversed(name)
+    rn = "".join(r)
+    if name == rn:
+        print(f'#{test_case} 1')
+    else:
+        print(f'#{test_case} 0')
