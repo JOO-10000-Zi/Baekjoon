34e79feb12b2195564461a9e94250bd4bc3c59d2
주세환 <j4615545@gmail.com>
2023-04-19 11:12:42 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    fact = 1
+    for i in range(1, 11):
+        fact *= i
+        if fact > n :
+            break 
+        answer = i
+        
+    return answer
