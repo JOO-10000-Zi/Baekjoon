196821f17e522ed5da82ae8d1aa04d008fbce910
주세환 <j4615545@gmail.com>
2023-04-20 13:38:52 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = []
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for i in my_string:
+        if i in numbers:
+            answer.append(int(i))
+    
+    
+    return sorted(answer)
